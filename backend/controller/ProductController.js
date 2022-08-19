c370ca7c4f074c0251bb3e43f66c96dc47b7084f
Chanuth10 <it20074418@my.sliit.lk>
2022-08-19 11:34:55 +0530

    Implement adding products backend api

--- backend/controller/ProductController.js
+++ backend/controller/ProductController.js
@@ -1,3 +1,16 @@
+const Product = require("../models/ProductModel.js");
+
+// create Product
+exports.createProduct = async (req, res, next) => {
+
+  const product = await Product.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    product
+  });
+};
+
 exports.getAllProducts = (req, res) => {
   res.status(200).json({
     message: "Router is Working fine.",
